f4592e52321641206b30e5a73350bed05ab97cfd
Ivor Peles <Ivor@inside-89-109.wireless.utoronto.ca>
2015-04-11 02:00:25 -0400

    Implemented meme selection

--- Backend/server.py
+++ Backend/server.py
@@ -1,20 +1,25 @@
 from flask import Flask, send_from_directory
+from os import listdir
+from random import choice
+
 app = Flask(__name__)
 app.debug = True
-URLs = []
 
 @app.route("/")
 def meme():
-    # randomly select URL from URLs.
-    # load associated image.
     
-    # serves image.
-    return send_from_directory('memes','a.jpg')
+    # randomly select URL from URLs.
+    target_meme = choice(get_memes())
+    
+    # serve image.
+    return send_from_directory('memes', target_meme)
 
 @app.route("/submit")
 def submitURL():
     return "Hello World!"
 
+def get_memes():
+    return listdir("./memes")
+
 if __name__ == "__main__":
     app.run()
-
