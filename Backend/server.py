3c6e8d5377105389a099073a4f420bb2eb81e304
Lawrence Wu <lawrence@lawrencewu.me>
2015-04-12 14:26:03 -0400

    Add config file for target meme directory

--- Backend/server.py
+++ Backend/server.py
@@ -1,20 +1,20 @@
-from flask import Flask, send_from_directory, render_template, request 
+from flask import Flask, send_from_directory, render_template, request
 from os import listdir
 from random import choice
 import urllib
 from string import ascii_lowercase
+import config
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/")
+@app.route("/meme")
 def meme():
-    
     # randomly select URL from URLs.
     target_meme = choice(get_memes())
-    
+
     # serve image.
-    return send_from_directory('memes', target_meme)
+    return send_from_directory(config.meme_directory, target_meme)
 
 @app.route("/admin")
 def admin():
@@ -24,17 +24,17 @@ def admin():
 def submit():
     if request.form["pass"] == "me2x":
         memeURL = request.form["url"]
-        randname = "./memes/" + randomword(8) + ".jpeg"
-        urllib.request.urlretrieve(memeURL, randname) 
+        randname = config.meme_directory + randomword(8) + ".jpeg"
+        urllib.urlretrieve(memeURL, randname)
         return "Meme submitted as: " + randname[8:]
     else:
         return "Setting Invalid, did you mean 'meme'?"
 
 def get_memes():
-    return listdir("./memes")
+    return listdir(config.meme_directory)
 
 def randomword(length):
    return ''.join(choice(ascii_lowercase) for i in range(length))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=80)
